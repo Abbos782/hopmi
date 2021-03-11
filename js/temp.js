59b7fef06d0e97bc75b517f81e136615d15ebf0d
Abbos782 <77330418+Abbos782@users.noreply.github.com>
2021-03-11 19:30:59 +0300

    add

---
+++ js/temp.js
@@ -0,0 +1,35 @@
+
+let a = 192, 
+
+
+while(a == 0){
+    
+    res+= a % 10;
+    a/= 10;
+    
+}
+console.log(res)
+
+// function digitsum(value){
+//     if(value<10){
+//         console.log(value)
+//         // return value;
+
+//     }
+//     else{
+        
+//         let a = 0
+//         let res = 0
+//         while(a < 10){
+        
+//            let  res +=value % 10;
+//              let  a = value /10;
+//              console.log(res)
+//         }
+       
+       
+        
+//     }
+// }
+
+// console.log(digitsum(1))
